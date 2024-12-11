141a8a98fd4544bd38b7779082fddccdb3157d05
Ayush Krishna Murthi <kmayush27399@gmail.com>
2024-12-11 13:36:13 +0530

    Refine matches and draw keypoints

--- slam.py
+++ slam.py
@@ -3,15 +3,33 @@ import cv2
 import numpy as np
 
 from display import Display
+from frame import Extractor
 
-W, H = 640, 480
+W, H = 1920//2, 1080//2
 disp = Display(W, H)
+extractor = Extractor()
+
+def process_frame(img):
+    img = cv2.resize(img, (W, H))
+    matches = extractor.extract(img)
+
+    # Draw matches
+    for pt1, pt2 in matches:
+        u1, v1 = map(lambda x: int(round(x)), pt1)
+        u2, v2 = map(lambda x: int(round(x)), pt2)
+        cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
+        cv2.line(img, (u1, v1), (u2, v2), color=(255, 0, 0))
+    disp.paint(img)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("videos/test_countryroad.mp4")
+
     while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            disp.paint(cv2.resize(frame, (W, H)))
+
+        # cap.read gives us the frame and a boolean value
+        flag, frame = cap.read()
+
+        if flag==True:
+            process_frame(frame)
         else:
             break
